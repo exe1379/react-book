f190403ecbb5e8cc7429e8dc5412358a133e1314
exe1379 <exe1379@gmail.com>
2025-05-14 14:26:44 +0800

    book

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <legend>新增書籍</legend>
+        <form>
+          id: <input name="id"/><p/>
+          書名: <input name="name"/><p/>
+          價格: <input name="price"/><p/>
+          數量: <input name="amount"/><p/>
+          出刊: <input type='checkbox' name="pub"/><p/>
+          <button type="submit">新增</button>
+        </form>
+          <h2>📖 書籍列表</h2>
+      <table border="1" cellPadding="4">
+        <thead>
+          <tr>
+          <th>ID</th>
+          <th>書名</th>
+          <th>價格</th>
+          <th>數量</th>
+          <th>出刊</th>
+          <th>操作</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+              <button type='button'>編輯</button>
+              <button type='button'>刪除</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
